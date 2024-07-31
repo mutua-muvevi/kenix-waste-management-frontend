47fafc5c4553c00baa1e213d930d6a77f9ff7d41
mutua-muvevi <samstore637@gmail.com>
2024-07-31 21:35:05 +0300

    addeds some social media links

--- src/components/sections/socialmedia.js
+++ src/components/sections/socialmedia.js
@@ -3,11 +3,11 @@ import Iconify from "../iconify/iconify"
 const SocialMediaIcons = [
 	{
 		icon: <Iconify icon="ic:outline-facebook"/>,
-		link: "https://www.facebook.com"
+		link: "https://www.facebook.com/profile.php?id=61563413109094&mibextid=ZbWKwL"
 	},
 	{
 		icon: <Iconify icon="simple-icons:x"/>,
-		link: "https://twitter.com"
+		link: "https://x.com/KenixwasteLtd"
 	},
 	{
 		icon: <Iconify icon="ri:instagram-fill"/>,
@@ -17,10 +17,10 @@ const SocialMediaIcons = [
 		icon: <Iconify icon="mdi:linkedin"/>,
 		link: "https://www.linkedin.com/company/kenix-waste-solutions-ltd/"
 	},
-	{
-		icon: <Iconify icon="basil:reddit-solid"/>,
-		link: "https://www.reddit.com"
-	},
+	// {
+	// 	icon: <Iconify icon="basil:reddit-solid"/>,
+	// 	link: "https://www.reddit.com"
+	// },
 ]
 
 export default SocialMediaIcons
